9a05df7e9b2efe4ecd12a6c51b0bd82ae4abd702
wang-zewen <65105157+wang-zewen@users.noreply.github.com>
2025-10-19 12:23:47 +0800

    Update speedtest.php

--- speedtest.php
+++ speedtest.php
@@ -6,7 +6,7 @@ header('Cache-Control: no-cache, no-store, must-revalidate');
 
 // 生成指定大小的随机数据用于测速
 // 默认 10MB
-$size = isset($_GET['size']) ? intval($_GET['size']) : 10;
+$size = isset($_GET['size']) ? intval($_GET['size']) : 50;
 $size = min($size, 100); // 最大 100MB
 $size = max($size, 1);   // 最小 1MB
 
